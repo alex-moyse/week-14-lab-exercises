79e59cf04e44a122efbf300a38dcbb9d974a14af
Alex <alex.moyse@warwick.ac.uk>
2023-02-20 09:59:45 +0000

    fixed linting errors

--- combined/index.js
+++ combined/index.js
@@ -5,6 +5,6 @@ const app = express();
 app.get("/:language", function (req, res) {
 	var greeting = hello.generateHelloString(req.params.language.toLowerCase());
 	res.send("<h1>" + greeting + "</h1>");
-})
+});
 
 app.listen(process.env?.PORT || 3000);
